7a2ee61d512f58dd75024e13c3528bf8430a2aa6
liujintaort <1109495668@qq.com>
2016-11-22 00:26:59 +0800

    kk

---
+++ js/ljt-shouye1.js
@@ -0,0 +1,54 @@
+$(function () {
+
+    //banner
+    $(".ljt-btn").click(function(){
+
+            $(".ljt-btn").removeClass("ljt-j1");
+
+
+        $(this).toggleClass("ljt-j1");
+
+       })
+
+var n=0;
+    var ss=setInterval(bo,3000)
+    function bo() {
+        n++;
+        if(n>= $(".ljt-banner-btn1").length){
+            n=0
+        };
+        $(".ljt-banner-btn1").css("background","transparent").eq(n).css("background","#2691fd")
+        $(".ljt-banner0").css({"display":"none",opacity:0,scale:0.7}).eq(n).css("display","block").transition({opacity:1,scale:1},800);
+    }
+
+$(".ljt-banner-btn1").mouseover(function () {
+
+    var index=$(this).index();
+    console.log(index)
+    $(".ljt-banner-btn1").css("background","transparent").eq(index).css("background","#2691fd")
+    $(".ljt-banner0").css({"display":"none",opacity:0,scale:0.7}).eq(index).css("display","block").transition({opacity:1,scale:1},500);
+
+})
+    $(".ljt-banner").hover(function () {
+        clearInterval(ss)
+    },function () {
+        ss=setInterval(bo,3000)
+    })
+
+   var jia=$(".ljt-ccv");
+    jia. click(function () {
+        $(".ljt-jia").slideDown(600);
+        $(".ljt-hj").css("display","block");
+
+    })
+    jia.mouseout(function () {
+        $(".ljt-jia").slideUp(200);
+        $(".ljt-hj").css("display","none");
+
+    })
+
+
+
+
+
+})
